3d0568b2f2d7bff56f5d3f7ff4282ecd53f131d8
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2022-10-17 16:12:41 +0200

    [fix]: update Camera architecture

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 # Modify this Procfile to fit your needs
-web: gunicorn main:app
+web: gunicorn -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:8080" main:app
--- camera.py
+++ camera.py
@@ -1,13 +1,11 @@
 import cv2
 
 class Camera():
-    def __init__(self):
-        self.video = cv2.VideoCapture(0)
-
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
+        self.video = cv2.VideoCapture(0)
         success, image = self.video.read()
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
